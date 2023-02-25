d8a47a0476abae73349dfe87fa2b94dcfd321de2
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2023-02-25 12:11:02 +0100

    'foundation'

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
-    
+
 DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '139.99.209.131',
-            'DB_NAME': 'retail_db',
-            'DB_USER': 'retail_user', # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
-            'DB_PASS': 'itversity' # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
+            'DB_HOST': '',
+            'DB_NAME': '',
+            'DB_USER': '', # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
+            'DB_PASS': '' # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
         },
 
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '139.99.209.131',
-            'DB_NAME': 'retail_db',
-            'DB_USER': 'retail_user', # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
-            'DB_PASS': 'itversity' # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
+            'DB_HOST': '',
+            'DB_NAME': '',
+            'DB_USER': '', # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
+            'DB_PASS': '' # to externalize, use 'setx or set' command in cmd to set environment variable, and access using 'os.environ.get('source_var_name')'
         }
     }
 }
